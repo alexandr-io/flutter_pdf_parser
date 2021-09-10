6144030e309df5b7e303dbc3f53c2f246a57653b
Khalas-animal <55802059+Khalas-animal@users.noreply.github.com>
2021-09-10 22:03:10 +0200

    add empty temporary widget

--- lib/flutter_pdf_parser.dart
+++ lib/flutter_pdf_parser.dart
@@ -1,25 +1,59 @@
 library flutter_pdf_parser;
 
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdf_parser/complete_object.dart';
 import 'package:flutter_pdf_parser/pagetree.dart';
 import 'parse_object.dart';
 
 /// A Calculator.
-class flutter_pdf {
-  String path;
+class Flutter_pdf {
   CompleteObject? completeObject;
-  int totalpages = 0;
-  flutter_pdf(this.path) {
-    File tmp = new File(path);
-    ParseObject parseobj = new ParseObject(tmp.readAsBytesSync());
-    completeObject = new CompleteObject(parseobj);
-    totalpages = completeObject!.rootTree!.getPageNumber();
+  int totalpages = 3;
+  Flutter_pdf(Uint8List bytes) {
+    // ParseObject parseobj = ParseObject(bytes);
+    // completeObject = CompleteObject(parseobj);
+    // totalpages = completeObject!.rootTree!.getPageNumber();
   }
-  getPageContent(int i) {
-    PageTree tree = completeObject!.rootTree!;
-    PageNode leaf = tree.getPage(i);
-    Content content = leaf.content!;
+  int getPageContent(int i) {
+    // PageTree tree = completeObject!.rootTree!;
+    // PageNode leaf = tree.getPage(i);
+    // Content content = leaf.content!;
+    return i;
+  }
+}
+
+class PDFPage extends StatelessWidget {
+  PDFPage(int index) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(width: 0.2, color: Colors.black),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Stack(),
+      ),
+    );
+  }
+}
+
+class PDFBook extends StatelessWidget {
+  Flutter_pdf? flutterpdf;
+
+  PDFBook(Uint8List bytes) {
+    flutterpdf = Flutter_pdf(bytes);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [for (var i = 0; i < flutterpdf!.totalpages; ++i) PDFPage(flutterpdf!.getPageContent(i))],
+    ));
   }
 }
