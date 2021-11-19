6272a1ec608d71b6ac523ad8eecb27bf3e7a8201
anon <anon>
2021-11-19 18:23:31 +0100

    change: switch to preprod

--- lib/flutter_pdf.dart
+++ lib/flutter_pdf.dart
@@ -4,28 +4,22 @@ import 'package:flutter_pdf_parser/flutter_bookmark.dart';
 import 'package:http/http.dart' as http;
 
 class AlexandrioAPIController {
-
   Future<void> postProgression(String token, String book, String library, String? progress) async {
-    var response = await http.post(
-        Uri.parse('https://library.alexandrio.cloud/library/$library/book/$book/progress'),
+    var response = await http.post(Uri.parse('https://library.preprod.alexandrio.cloud/library/$library/book/$book/progress'),
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $token',
         },
-        body: jsonEncode({"progress": progress})
-    );
+        body: jsonEncode({"progress": progress}));
 
     if (response.statusCode != 200) throw 'Coudn\'t update progress';
   }
 
   Future<List<List<String>>> getAllUserData(String token, String libraryId, String bookId) async {
-    var response = await http.get(
-        Uri.parse('https://library.alexandrio.cloud/library/$libraryId/book/$bookId/data'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token',
-        }
-    );
+    var response = await http.get(Uri.parse('https://library.preprod.alexandrio.cloud/library/$libraryId/book/$bookId/data'), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
 
     List<List<String>> bookmarkList = [];
 
@@ -49,43 +43,22 @@ class AlexandrioAPIController {
   }
 
   Future<void> deleteAllUserData(String token, String libraryId, String bookId) async {
-    var response = await http.delete(
-        Uri.parse('https://library.alexandrio.cloud/library/$libraryId/book/$bookId/data'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token'
-        }
-    );
+    var response = await http.delete(Uri.parse('https://library.preprod.alexandrio.cloud/library/$libraryId/book/$bookId/data'), headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer $token'});
 
     if (response.statusCode != 200) throw 'Couldn\'t delete all data';
   }
 
   Future<void> postUserData(String token, String libraryId, String bookId, String type, String description, String name, String offset) async {
-    var response = await http.post(
-        Uri.parse('https://library.alexandrio.cloud/library/$libraryId/book/$bookId/data'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token'
-        },
-        body: jsonEncode({
-          "name": description.isEmpty ? type : description,
-          "offset": offset,
-          "description": description,
-          "type": type
-        })
-    );
+    var response = await http.post(Uri.parse('https://library.preprod.alexandrio.cloud/library/$libraryId/book/$bookId/data'), headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer $token'}, body: jsonEncode({"name": description.isEmpty ? type : description, "offset": offset, "description": description, "type": type}));
 
     if (response.statusCode != 201) throw 'Couldn\'t create data';
   }
 
   void deleteUserData(String token, String libraryId, String bookId, String dataId) async {
-    var response = await http.delete(
-        Uri.parse('https://library.alexandrio.cloud/library/$libraryId/book/$bookId/data/$dataId'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $token',
-        }
-    );
+    var response = await http.delete(Uri.parse('https://library.preprod.alexandrio.cloud/library/$libraryId/book/$bookId/data/$dataId'), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
 
     if (response.statusCode != 200) throw 'Couldn\'t delete data';
   }
